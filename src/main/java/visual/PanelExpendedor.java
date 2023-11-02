dcfe1ddb77f23fbdc49833dc837d95b9b797207c
jgodoy2022@ <jgodoy2022@udec.cl>
2023-11-02 11:41:19 -0300

    fix: PanelExpendedor cantidad de productos, Expendedor nuevo metodo

--- src/main/java/visual/PanelExpendedor.java
+++ src/main/java/visual/PanelExpendedor.java
@@ -1,6 +1,7 @@
 package visual;
 
 import org.example.Expendedor;
+import org.example.TipoProducto;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,9 +17,10 @@ public class PanelExpendedor extends JPanel implements ActionListener {
     int XVelocity = 1;
     int y = 0;
     int x = 0;
-
+    private Expendedor expendedor;
 
     public PanelExpendedor(Expendedor expendedor){
+        this.expendedor=expendedor;
         this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
         this.setBackground(Color.BLACK);
         coca = new ImageIcon("src/main/java/visual/cocacola.png").getImage();
@@ -28,18 +30,33 @@ public class PanelExpendedor extends JPanel implements ActionListener {
         super8 = new ImageIcon("src/main/java/visual/supero8.png").getImage();
 
         timer = new Timer(10, this);
-        //timer.start();
+        timer.start();
     }
 
     public void paint(Graphics g){
         super.paint(g);
+        g.setColor(Color.green);
+        g.fillRect(0, 0, 600, 500);
         Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(coca, x, y, null);
-        g2D.drawImage(fanta, x, y+100, null);
-        g2D.drawImage(sprite, x+16, y+200, null);
-        g2D.drawImage(snickers, x, y+320, null);
-        g2D.drawImage(super8, x, y+350, null);
+        for (int i = 0; i < expendedor.getCantidadProducto(TipoProducto.COCA); i++) {
+            g2D.drawImage(coca, x, y + i * 100, null);
+        }
 
+        for (int i = 0; i < expendedor.getCantidadProducto(TipoProducto.FANTA); i++) {
+            g2D.drawImage(fanta, x+i*100, y + 100, null);
+        }
+
+        for (int i = 0; i < expendedor.getCantidadProducto(TipoProducto.SPRITE); i++) {
+            g2D.drawImage(sprite, x +i*100+ 16, y + 200, null);
+        }
+
+        for (int i = 0; i < expendedor.getCantidadProducto(TipoProducto.SNICKERS); i++) {
+            g2D.drawImage(snickers, x+i*100, y + 320 , null);
+        }
+
+        for (int i = 0; i < expendedor.getCantidadProducto(TipoProducto.SUPER8); i++) {
+            g2D.drawImage(super8, x+i*100, y + 350, null);
+        }
     }
 
     @Override
