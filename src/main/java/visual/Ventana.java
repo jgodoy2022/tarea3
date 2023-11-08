df037c1d4270b093c5a6dc609a623bb8bbb8d50f
jgodoy2022@ <jgodoy2022@udec.cl>
2023-11-08 17:46:50 -0300

    docs: documentacion Jframe Ventana

--- src/main/java/visual/Ventana.java
+++ src/main/java/visual/Ventana.java
@@ -5,10 +5,21 @@ import org.example.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * El Jframe ventana representa la ventana de la interfaz grafica, se utiliza para poder mostrar la interfaz grafica del codigo
+ * @author Gabriela Isidora Zambrano Novoa
+ * @author Joaquin Alejandro Godoy Vergara
+ */
 public class Ventana extends JFrame {
+    /** Referencia de al panel principal el cual contiene a el resto de paneles*/
     private PanelPrincipal mypanel;
+
+    /** Referencia al expendedor utilizado en la venta*/
     private Expendedor expendedor;
 
+    /**
+     * Constructor inicializa la ventana y configura sus propiedades
+     */
     public Ventana(){
         expendedor=new Expendedor(6);
         mypanel = new PanelPrincipal(expendedor);
